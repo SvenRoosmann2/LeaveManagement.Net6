1d69951d22dde5e1492bc00c9fbd4e89f48a1ff6
SvenRoosmann2 <80288023+SvenRoosmann2@users.noreply.github.com>
2023-01-02 17:45:31 +0100

    Addead datatables

--- LeaveManagement.Web/Data/ApplicationDbContext.cs
+++ LeaveManagement.Web/Data/ApplicationDbContext.cs
@@ -3,8 +3,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace LeaveManagement.Web.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Employee>
     {
+
+        public DbSet<LeaveAllocation> LeaveAllocations { get; set; }
+        public DbSet<LeaveType> LeaveTypes { get; set; }
+
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
