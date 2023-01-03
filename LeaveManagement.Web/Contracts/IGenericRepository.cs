beed439dd3ae2387fe26453c7ee27ef6e91c9122
SvenRoosmann2 <80288023+SvenRoosmann2@users.noreply.github.com>
2023-01-03 18:47:59 +0100

    changes

---
+++ LeaveManagement.Web/Contracts/IGenericRepository.cs
@@ -0,0 +1,15 @@
+﻿namespace LeaveManagement.Web.Contracts
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetAsync(int? id);
+        Task<List<T>> GetAllAsync();
+        Task<T> AddAsync(T entity);
+        Task<int> GetCountAsync();
+        Task DeleteAsync(int id);
+        Task<bool> Exists(int id);
+        Task UpdateAsync(T entity);
+        
+
+    }
+}
